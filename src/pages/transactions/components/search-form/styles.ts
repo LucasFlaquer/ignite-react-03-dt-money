7a47ec54a3a9fbd68ff4e202af2c98b73a1e4060
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-12-17 00:00:52 -0300

    transactions search form

---
+++ src/pages/transactions/components/search-form/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const Container = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+    border-radius: 6px;
+    border: 0;
+    background-color: ${({ theme }) => theme['gray-900']};
+    color: ${({ theme }) => theme['gray-300']};
+    padding: 1rem;
+
+    &::placeholder {
+      color: ${({ theme }) => theme['gray-500']};
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.75rem;
+    border: 0;
+    padding: 1rem;
+    background: transparent;
+    border: 1px solid ${(props) => props.theme['green-300']};
+    color: ${(props) => props.theme['green-300']};
+    font-weight: bold;
+    border-radius: 6px;
+
+    &:hover {
+      background: ${({ theme }) => theme['green-500']};
+      border: 1px solid ${({ theme }) => theme['green-500']};
+      color: ${(props) => props.theme.white};
+      transition:
+        backgroud-color 0.2s,
+        color 0.2s,
+        border 0.2s;
+    }
+  }
+`
