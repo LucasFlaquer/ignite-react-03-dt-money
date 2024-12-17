5c9159cdd166fb74d6d7b1b6d6221b71e6915856
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-12-16 23:50:39 -0300

    table transactions

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from '../../components/header'
 import { Summary } from '../../components/summary'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Deenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>14/12/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Date</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 12.00,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>14/12/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
