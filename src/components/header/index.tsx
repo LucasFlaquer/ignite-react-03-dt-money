ec3757706f8d40e432e77b0ea4b8bdfa10f8799c
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-12-18 15:24:27 -0300

    transaction button type

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,20 @@
 import { Container, Content, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../new-transaction-modal'
 
 export function Header() {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </Content>
     </Container>
   )
